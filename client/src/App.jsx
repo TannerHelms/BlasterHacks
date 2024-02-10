e4b1447f328689cc40d3f597f616cd891d698944
Ethan Ford <ethanfordj5@gmail.com>
2024-02-09 23:12:43 -0700

    prettier format

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { io } from "socket.io-client";
+import Button from "./componets/button/button";
 import { Quote } from "./requests/quote.js";
-import { TextSearch } from "./requests/textSearch";
+import { useGeo } from "./hooks/geoFunc.js";
 function App() {
+  const { location, fetchData } = useGeo();
+  const handleButtonClick = () => {
+    fetchData();
+  };
+
   return (
     <>
       <p>hello, world</p>
-      <p>
-        <TextSearch />
-      </p>
+      <div>
+        <h2>Your Location:</h2>
+        <p>Latitude: {location.latitude}</p>
+        <p>Longitude: {location.longitude}</p>
+        <p>City: {location.city}</p>
+        <p>State: {location.state}</p>
+        <button onClick={handleButtonClick}>Get Location</button>
+      </div>
     </>
   );
 }
