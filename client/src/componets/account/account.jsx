e3d2253b653406e0951fc6017dfcde48e204935c
Ethan Ford <ethanfordj5@gmail.com>
2024-02-10 23:46:13 -0700

    started adding account content

--- client/src/componets/account/account.jsx
+++ client/src/componets/account/account.jsx
@@ -1,9 +1,18 @@
 import classes from './account.module.css';
+import { getAuth } from 'firebase/auth';
 
 export default function Account() {
+    const user = getAuth().currentUser.email.split('@')[0]
+    const email = getAuth().currentUser.email
     return (
         <div className={classes.account}>
-            <h2>ACCOUNTS</h2>
+            <h2>ACCOUNT</h2>
+            <div className={classes.tile}>
+                <div className={classes.second}>
+                    <p> {user} </p>
+                    <p> {email} </p>
+                </div>
+            </div>   
         </div>
     )
 }
